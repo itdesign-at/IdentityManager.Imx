a2f8687994e150e034b0b279b1b9b954e32df656
Hao Dong <hao.dong@itdesign.at>
2022-11-02 15:48:15 +0100

    Remove menu items

--- imxweb/projects/ccc-itd-lib/src/lib/menu/menu.service.ts
+++ imxweb/projects/ccc-itd-lib/src/lib/menu/menu.service.ts
@@ -1,28 +1,35 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
-import { ProjectConfig } from 'imx-api-qbm';
+import {ProjectConfig} from 'imx-api-qbm';
 
-import {MenuFactory, MenuItem, MenuService} from 'qbm';
+import {MenuItem, MenuService} from 'qbm';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItdMenuService extends MenuService {
 
-  private DisabledMenuItems = ["ROOT_Responsibilities", "QER_Requests_NewRequest", "QER_Request_RequestTemplates"];
+  private disabledMenuItems = ['ROOT_Responsibilities', 'QER_Requests_NewRequest', 'QER_Request_RequestTemplates', 'QER_Request_RequestHistory'];
 
   public getMenuItems(
     preProps: string[],
     groups: string[],
     allowEmpty: boolean = false,
     projectConfig?: ProjectConfig): MenuItem[] {
-    const menuItems = super.getMenuItems(preProps, groups, allowEmpty, projectConfig);
+    let menuItems = super.getMenuItems(preProps, groups, allowEmpty, projectConfig);
 
+    const filterDisabledItems = (item: MenuItem) => {
+      const disabled = this.disabledMenuItems.includes(item.id);
+      if (disabled) {
+        return false;
+      }
+      if (item.items) {
+        item.items = item.items.filter(filterDisabledItems);
+      }
+      return true;
+    };
 
-    console.log("getMenuItems")
-    console.log(menuItems)
-
+    menuItems = menuItems.filter(filterDisabledItems);
     return menuItems;
   }
-
 }
